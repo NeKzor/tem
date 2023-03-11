b19d85c87212a55d28e406280c535997e596262f
NeKz <nekzor@users.noreply.github.com>
2023-03-11 03:27:02 +0100

    Refactor offsets

--- src/Offsets.hpp
+++ src/Offsets.hpp
@@ -8,25 +8,27 @@
 
 namespace Offsets {
 
+typedef uint32_t vtable_offset_t, object_offset_t, function_offset_t;
+
 // UE3
-extern int g_Objects;
-extern int g_Names;
-extern int g_Engine;
-extern int g_Renderer;
-extern int ProcessEvent;
-extern int GetTeamColor;
-extern int ConsoleCommand;
+const object_offset_t g_Objects = 0x2278E7C;
+const object_offset_t g_Names = 0x2278E4C;
+const object_offset_t g_Engine = 0x2286CA8;
+const object_offset_t g_Renderer = 0x227DDF0;
+const vtable_offset_t ProcessEvent = 62;
+const function_offset_t GetTeamColor = 0x1439DF0;
+const vtable_offset_t ConsoleCommand = 71;
 
 // GFWL
-extern int xlive_memory_check;
-extern int xlive_main_thread_start_address;
+const function_offset_t xlive_memory_check = 0xF370C;
+const function_offset_t xlive_main_thread_start_address = 0x146AB8;
 
 // GridGame
-extern int forced_window_minimize;
-extern int save_filename;
-extern int pg_save_load;
+const function_offset_t forced_window_minimize = 0x13587B8;
+const object_offset_t save_filename = 0x21AA580;
+const object_offset_t pg_save_load = 0x22B571C;
 
 // D3D9
-extern int Reset;
-extern int Present;
+const vtable_offset_t Reset = 16;
+const vtable_offset_t Present = 17;
 }
